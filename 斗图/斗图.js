d8160d96b378d90459fabf04c691cae9f4309b76
no-name <40132433+CodedArtist@users.noreply.github.com>
2019-05-29 20:30:37 +0800

    Update 斗图.js

--- 斗图/斗图.js
+++ 斗图/斗图.js
@@ -1,104 +1,91 @@
-/*
+// https://github.com/CodedArtist/JSBox/tree/master/斗图
 
-*/
+const imgCount = 16;  // 可选值 4、8、12、16
+
+if($app.env!=$env.keyboard){
+  $ui.alert("本脚本只能运行在键盘扩展中");
+  $app.close();
+}
+
 $keyboard.barHidden = true;
-$ui.loading(true);
 
 var query = '';
 var start = 0;
-// 每次展示的图片个数。因为 matrix 是 4 列的，所以
-var imgCount = 16;
 
 $ui.render({
+  props: {
+    navBarHidden: 1,
+    statusBarStyle: 1
+  },
+  views: [{
+    type: "matrix",
     props: {
-        navBarHidden: 1,
-        statusBarStyle: 1
+      id: "imgs",
+      square: true,
+      columns: 4,
+      spacing: 1,
+      template: [{
+        type: "image",
+        layout: (make) => make.top.bottom.right.left.inset(0)
+      }]
     },
-    views: [{
-        type: "matrix",
-        props: {
-            id: "imgs",
-            itemHeight: $device.info.screen.width / 4,
-            columns: 4,
-            spacing: 1,
-            template: [{
-                type: "image",
-                layout: (make) => make.top.bottom.right.left.inset(0)
-            }]
-        },
-        layout: (make) => make.top.bottom.left.right.inset(0),
-        events: 
-        {
-            didSelect: function (sender, indexPath, data) {
-                $keyboard.moveCursor(1);
-                let t = setInterval(() => {
-                    $keyboard.moveCursor(1);
-                    if ($keyboard.hasText) {
-                        $keyboard.delete()
-                    }
-                    else{
-                        clearInterval(t)
-                    }
-                }, 10);
-                $http.get({
-                    url: data.image.src,
-                    handler: function(rsp) {
-                        $share.wechat(rsp.rawData); // 分享至 微信
-                        // $share.qq(rsp.rawData);  // 分享至 QQ
-                        // $share.tim(rsp.rawData); // 分享至 TIM
-                        // $share.sheet(rsp.rawData);   // 系统分享面板
-
-                        $keyboard.next();
-                    }
-                });
-            },
-            didLongPress: () => search(),
-            ready: () => {
-                // 必须要有此延迟
-                $delay(0.01, () => {
-                    if($keyboard.hasText){
-                        $keyboard.getAllText( (text) => {
-                            query = text; 
-                            search();
-                        })
-                    }
-                    else{
-                        $keyboard.next()
-                    }
-                });
-            }}
-    }, {
-        type: "spinner",
-        props: {
-            loading: false
-        },
-        layout: (make, view) => make.center.equalTo(view.super)
+    layout: (make) => make.top.bottom.left.right.inset(0),
+    events: {
+      didSelect: (sender, indexPath, data) => {
+        let t = setInterval(() => {
+          $keyboard.moveCursor(1);
+          if ($keyboard.hasText) $keyboard.delete()
+          else clearInterval(t)
+        }, 10);
+        $device.taptic(0);
+        $('spinner').loading = true;
+        $http.get(data.image.src,)
+          .then(rsp => {
+            // 选择其中一个，注释另外两个。
+            $share.wechat(rsp.rawData); // 分享至 微信
+            // $share.qq(rsp.rawData);  // 分享至 QQ
+            // $share.tim(rsp.rawData); // 分享至 TIM
+            
+            $keyboard.next();
+          });
+      },
+      didLongPress: () => {
+        $device.taptic(0);
+        $('spinner').loading = true;
+        search();
+      },
+      ready: () => {
+        // 必须要有此延迟
+        $delay(0.1, () => {
+          if($keyboard.hasText){
+            $keyboard.getAllText( (text) => {
+              query = text + '表情'; 
+              search();
+            })
+          }
+          else{
+            $keyboard.next()
+          }
+        });
+      }
+    }
+  }, {
+    type: "spinner",
+    props: {
+      loading: true
+    },
+    layout: (make, view) => make.center.equalTo(view.super)
     }]
 });
  
 function search() {
-//    let payload = {
-//        query: `${$text.URLEncode(query)}%20${$text.URLEncode("表情")}`,
-//        st: 5,
-//        start: 0,
-//        xml_len: 16,
-//        mode: 1,
-//        moode: 0,
-//        dm: 0
-//    }
-    
-    $http.get({
-        url: `https://pic.sogou.com/pics/json.jsp?query=${$text.URLEncode(query)}%20${$text.URLEncode("表情")}&st=5&start=${start}&xml_len=${imgCount}`,
-        handler: (resp) => {
-            start+=16;
-            // $('spinner').loading = false;
-            $ui.loading(false);
-            let items = resp.data.items;
-            let data = [];
-            for (let i in items) data.push({ image: { src: items[i].picUrl } });
-            $('imgs').data = data;
-            
-        }
+  $http.get(`https://pic.sogou.com/pics/json.jsp?query=${$text.URLEncode(query)}&st=5&start=${start}&xml_len=${imgCount}`)
+    .then(rsp => {
+      start+=imgCount;
+      $('spinner').loading = false;
+      let items = rsp.data.items;
+      let data = [];
+      for (let i in items) data.push({ image: { src: items[i].picUrl } });
+      $('imgs').data = data;
     })
 }
-
